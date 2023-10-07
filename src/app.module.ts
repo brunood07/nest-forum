97ef1ab72b41bb57a7654361d528089930da9460
brunoodlighthouse <bruno.domingues@lighthouseit.com.br>
2023-10-07 17:56:21 -0300

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
+  controllers: [CreateAccountController],
   providers: [PrismaService],
 })
 export class AppModule {}
